0b61c0b66b62cc1b5eec540d4e4b2c870f04a153
Dudley Dickerson <littledudz@yahoo.com>
2017-03-20 23:33:49 -0500

    404 beginnings

---
+++ wordpress/wp-content/themes/EMC/404.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <img src="support/images/emc-birmingham-logo-iso-white.png" alt="emc logo" style="height: 90px;">
+    <div class="right-side">
+        <div class="404message">
+            <h2 class="">Oh no! It looks like you have hit a 404 error!</h2>
+            <p class="">Double check your login or head back to the homepage. If you continue to get this page, use our contact us form above or give us a call.</h3>
+        </div>
+        <div class="bottom_info">
+            <div class="contact_us">
+                <h3>Contact Us</h3>
+                <p>It looks like you have run into a problem. If you think it was a rando occouranc, keep browsing. Otherwise, contact us to fix it.</p>
+                <a href="" class="contact_us_button">Contact Us</a>
+                <h3>Give Us A Ring</h3>
+                <p>Looking to work with Enviro Manangement Cormoration for your next project? We would love to hear more and see how we can help. <strong>Call us at (205) 951-3400.</strong></p>
+            </div>
+            <div class="links">
+                <h3>Looking For Something?</h3>
+                <p>What you are loking for doesn't seem to exist, yet. Try one of the pages below to get back on your way.</p>
+                <ul>
+                    <li class="404bullets">1. <a class="404links">Our Homepage</a></li>
+                    <li class="404bullets">2. <a class="404links">Our Blog</a></li>
+                    <li class="404bullets">3. <a class="404links">Consulting Services</a></li>
+                    <li class="404bullets">4. <a class="404links">Field & Laboratory Operations</a></li>
+                    <li class="404bullets">5. <a class="404links">Cooling Tower Water Treatment</a></li>
+                    <li class="404bullets">6. <a class="404links">Septic Tank and Grease Trap Cleaning</a></li>
+                    <li class="404bullets">7. <a class="404links">Water Treatment Plant Operations</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
